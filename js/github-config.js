b4536791b9457deb6666ec13db6937fb0ac10e29
imnexerio <imnexerio@gmail.com>
2025-05-20 01:31:35 +0530

    env setup t9

--- js/github-config.js
+++ js/github-config.js
@@ -10,7 +10,7 @@ window.GitHubConfig = (function() {
     // Private GitHub credentials
     const _username = 'imnexerio';
     // replace your token here or leave it empty to use public access or use github environment variables
-    const _token = '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';
+    const _token = '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';
     
     // Configuration options
     const _config = {
